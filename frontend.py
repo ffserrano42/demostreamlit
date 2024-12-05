4e12e74fab4b9845902bf0fea0d6eeb482d04386
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-12-04 20:28:04 -0500

    cambios en la app

--- frontend.py
+++ frontend.py
@@ -6,7 +6,19 @@ import numpy as np
 from ultralytics import YOLO
 
 # Cargar modelo YOLO
-#model_proyecto = YOLO("best_model_sa_1.pt")  
-#model_proyecto_aug = YOLO("best_proyecto_conaumentacion.pt")
+
+@st.cache_data
+def load_model_sa():
+	  return YOLO("best_model_sa_1.pt")
+
+@st.cache_data
+def load_model_ca():
+	  return YOLO("best_proyecto_conaumentacion.pt")
+
+
+# Cargar modelo YOLO
+model_proyecto=load_model_sa()  #este es el modelo entrenado sin datos aumentados.
+#model_proyecto_aug=YOLO("best_proyecto_v11_6.pt") # este es el modelo entrenado con datos aumentados
+model_proyecto_aug=load_model_ca() # este es el modelo entrenado con datos aumentados
 
 st.text("seleccione una imagen y presione el botón de 'Procesar' para detectar las plantas de papa ")
