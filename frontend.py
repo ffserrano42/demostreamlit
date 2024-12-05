30a690b17ccc0c2275a05600780699f461eadfb2
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-12-04 20:25:09 -0500

    demo

--- frontend.py
+++ frontend.py
@@ -6,7 +6,7 @@ import numpy as np
 from ultralytics import YOLO
 
 # Cargar modelo YOLO
-model_proyecto = YOLO("best_model_sa_1.pt")  
-model_proyecto_aug = YOLO("best_proyecto_conaumentacion.pt")
+#model_proyecto = YOLO("best_model_sa_1.pt")  
+#model_proyecto_aug = YOLO("best_proyecto_conaumentacion.pt")
 
 st.text("seleccione una imagen y presione el botón de 'Procesar' para detectar las plantas de papa ")
